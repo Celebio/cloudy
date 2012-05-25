2d3e858ca283bbc3208f4aab36b0256ef40fbc0b
Onur Celebi <on.celebi@gmail.com>
2012-05-25 19:16:26 +0300

    more difficult

--- p4.py
+++ p4.py
@@ -188,7 +188,7 @@ def think2(board, level, curPlayer, bestAction):
     
 
 def think(board, level, curPlayer):
-    if level == 5:
+    if level == 7:
         return (0, -1)
     
     #print "considering board;"
